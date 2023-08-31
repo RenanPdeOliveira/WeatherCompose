0bac7ce6a97426ea1b0f66f8bb0211e48b4eca24
Renan Pires de Oliveira <renanpires919mx@gmail.com>
2023-08-31 04:46:43 -0300

    Dependency injection with dagger-hilt implemented

---
+++ app/src/main/java/com/example/weathercompose/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.example.weathercompose.di
+
+import android.app.Application
+import com.example.weathercompose.data.remote.WeatherApi
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideWeatherApi(): WeatherApi {
+        return Retrofit.Builder()
+            .baseUrl("https://api.open-meteo.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFusedLocationProviderClient(app: Application): FusedLocationProviderClient {
+        return LocationServices.getFusedLocationProviderClient(app)
+    }
+}
